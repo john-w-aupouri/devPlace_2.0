438ad08af7b5790eb23e437591b7bdf4d07d1498
john_wirihana_aupouri <johnaupouri963@gmail.com>
2021-03-15 10:22:35 +1300

    refactored post components

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import { Link, Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Link, Redirect } from 'react-router-dom';
 
 const Landing = ({ isAuthenticated }) => {
   if (isAuthenticated) {
