d4d15d42387af2241c34073b68ad7033ed113aa6
john_wirihana_aupouri <johnaupouri963@gmail.com>
2021-03-15 10:24:07 +1300

    refactored posts components

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -1,9 +1,9 @@
-import React, { Fragment } from 'react';
+import { Fragment } from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import formatDate from '../../utils/formatDate';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { addLike, removeLike, deletePost } from '../../actions/post';
+import formatDate from '../../utils/formatDate';
 
 const PostItem = ({
   addLike,
@@ -13,47 +13,44 @@ const PostItem = ({
   post: { _id, text, name, avatar, user, likes, comments, date },
   showActions
 }) => (
-  <div className="post bg-white p-1 my-1">
+  <div className='post bg-white p-1 my-1'>
     <div>
       <Link to={`/profile/${user}`}>
-        <img className="round-img" src={avatar} alt="" />
+        <img className='round-img' src={avatar} alt='' />
         <h4>{name}</h4>
       </Link>
     </div>
     <div>
-      <p className="my-1">{text}</p>
-      <p className="post-date">Posted on {formatDate(date)}</p>
+      <p className='my-1'>{text}</p>
+      <p className='post-date'>Posted on {formatDate(date)}</p>
 
       {showActions && (
         <Fragment>
           <button
             onClick={() => addLike(_id)}
-            type="button"
-            className="btn btn-light"
-          >
-            <i className="fas fa-thumbs-up" />{' '}
+            type='button'
+            className='btn btn-light'>
+            <i className='fas fa-thumbs-up' />{' '}
             <span>{likes.length > 0 && <span>{likes.length}</span>}</span>
           </button>
           <button
             onClick={() => removeLike(_id)}
-            type="button"
-            className="btn btn-light"
-          >
-            <i className="fas fa-thumbs-down" />
+            type='button'
+            className='btn btn-light'>
+            <i className='fas fa-thumbs-down' />
           </button>
-          <Link to={`/posts/${_id}`} className="btn btn-primary">
+          <Link to={`/posts/${_id}`} className='btn btn-primary'>
             Discussion{' '}
             {comments.length > 0 && (
-              <span className="comment-count">{comments.length}</span>
+              <span className='comment-count'>{comments.length}</span>
             )}
           </Link>
           {!auth.loading && user === auth.user._id && (
             <button
               onClick={() => deletePost(_id)}
-              type="button"
-              className="btn btn-danger"
-            >
-              <i className="fas fa-times" />
+              type='button'
+              className='btn btn-danger'>
+              <i className='fas fa-times' />
             </button>
           )}
         </Fragment>
--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from 'react';
+import { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import PostItem from './PostItem';
@@ -12,12 +12,12 @@ const Posts = ({ getPosts, post: { posts } }) => {
 
   return (
     <Fragment>
-      <h1 className="large text-primary">Posts</h1>
-      <p className="lead">
-        <i className="fas fa-user" /> Welcome to the community
+      <h1 className='large text-primary'>Posts</h1>
+      <p className='lead'>
+        <i className='fas fa-user' /> Welcome to the community
       </p>
       <PostForm />
-      <div className="posts">
+      <div className='posts'>
         {posts.map((post) => (
           <PostItem key={post._id} post={post} />
         ))}
