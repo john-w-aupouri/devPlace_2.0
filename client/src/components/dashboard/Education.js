9c4151e7ca0d60649368943323677562eeac6f30
john_wirihana_aupouri <johnaupouri963@gmail.com>
2021-03-15 10:18:28 +1300

    refactored dashboard components

--- client/src/components/dashboard/Education.js
+++ client/src/components/dashboard/Education.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { deleteEducation } from '../../actions/profile';
@@ -8,15 +8,14 @@ const Education = ({ education, deleteEducation }) => {
   const educations = education.map((edu) => (
     <tr key={edu._id}>
       <td>{edu.school}</td>
-      <td className="hide-sm">{edu.degree}</td>
+      <td className='hide-sm'>{edu.degree}</td>
       <td>
         {formatDate(edu.from)} - {edu.to ? formatDate(edu.to) : 'Now'}
       </td>
       <td>
         <button
           onClick={() => deleteEducation(edu._id)}
-          className="btn btn-danger"
-        >
+          className='btn btn-danger'>
           Delete
         </button>
       </td>
@@ -25,13 +24,13 @@ const Education = ({ education, deleteEducation }) => {
 
   return (
     <Fragment>
-      <h2 className="my-2">Education Credentials</h2>
-      <table className="table">
+      <h2 className='my-2'>Education Credentials</h2>
+      <table className='table'>
         <thead>
           <tr>
             <th>School</th>
-            <th className="hide-sm">Degree</th>
-            <th className="hide-sm">Years</th>
+            <th className='hide-sm'>Degree</th>
+            <th className='hide-sm'>Years</th>
             <th />
           </tr>
         </thead>
